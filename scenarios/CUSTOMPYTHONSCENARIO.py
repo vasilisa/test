68e5a6dfb5ae6df88ab4eefa5093f685bcfa2e56
admin <admin>
2024-11-06 07:51:41 +0000

    Saved scenario MLOPSDEMO.Custom Python Scenario (id: CUSTOMPYTHONSCENARIO)

--- scenarios/CUSTOMPYTHONSCENARIO.py
+++ scenarios/CUSTOMPYTHONSCENARIO.py
@@ -1,5 +1,6 @@
 # This sample code helps you get started with the custom scenario API.
 #For more details and samples, please see our Documentation
+import dataiku 
 from dataiku.scenario import Scenario
 
 # The Scenario object is the main handle from which you initiate steps
@@ -8,17 +9,18 @@ scenario = Scenario()
 # A few example steps follow
 
 # Building a dataset
-scenario.build_dataset("customers_prepared", partitions="2015-01-03")
+scenario.build_dataset("transactions_MES_filtered_scored", partitions="NP")
 
-# Controlling the train of a dataset
-train_ret = scenario.train_model("uSEkldfsm")
-trained_model = train_ret.get_trained_model()
-performance = trained_model.get_new_version_metrics().get_performance_values()
-if performance["AUC"] > 0.85:
-    trained_model.activate_new_version()
 
+client     = dataiku.api_client()
+project    = client.get_default_project()
+
+# get the email from the variable
+recipients = project.get_variables()["standard"]["recipient_list"]
+
+    
 # Sending custom reports
 sender = scenario.get_message_sender("mail-scenario", "local-mail") # A messaging channel
-sender.set_params(sender="dss@company.com", recipient="data-scientists@company.com")
+sender.set_params(sender="dss@company.com", recipient=recipients)
 
-sender.send(subject="The scenario is doing well", message="All is good")
+sender.send(subject="The scenario is doing well", message="New data scored")
